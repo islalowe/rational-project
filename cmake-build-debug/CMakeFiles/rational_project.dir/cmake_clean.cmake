f686556d9f99bf6fb9e9a9ec7db95a30609d9d8f
islalowe <lowek3@spu.edu>
2024-07-29 13:32:34 -0700

    open

---
+++ cmake-build-debug/CMakeFiles/rational_project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rational_project.dir/main.cpp.o"
+  "CMakeFiles/rational_project.dir/main.cpp.o.d"
+  "CMakeFiles/rational_project.dir/rational.cpp.o"
+  "CMakeFiles/rational_project.dir/rational.cpp.o.d"
+  "rational_project"
+  "rational_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rational_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/rational_project.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for rational_project.
+# This may be replaced when dependencies are built.
